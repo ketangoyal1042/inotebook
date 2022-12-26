5a58165ff1048e3abf3b8cab82432d24f8a9c267
Sahil Goyal <ketangoyal1042@gmail.com>
2022-12-26 17:31:48 +0530

    Till 54, Till we have created all the endpoints & done with node

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,14 +4,14 @@ const getchusermiddleware = require('../middleware/fetchuser');
 const Note = require('../models/Note');
 const { body, validationResult } = require('express-validator');
 
-// get user notes using: GET "/api/auth/register".
+// get user notes using: GET "/api/notes/register".
 router.get('/fetchnotes', getchusermiddleware, async (req, res) => {
     userid = req.id;
     const notes = await Note.find({ user: userid });
     res.json(notes);
 });
 
-// create user notes using: POST "/api/auth/addnote".
+// create user notes using: POST "/api/notes/addnote". LOGIN Required
 router.post('/addnote', getchusermiddleware, [
     body('title', "Please provide title of min length 5").isLength({ min: 5 }),
     body('description', "Enter a Valid description of min length 5").isLength({ min: 5 }),    // these 2 validations are being added using express-validator packeage
@@ -34,6 +34,64 @@ router.post('/addnote', getchusermiddleware, [
 
         res.json(note);
     } catch (error) {
+        res.status(500).send("Some internal Server Error occurred, error: " + error.message);
+    }
+});
+
+// Update user notes using: PUT "/api/notes/updatenode". LOGIN Required
+router.put('/updatenode/:nid', getchusermiddleware, async (req, res) => {
+    const { title, description, tag } = req.body;
+
+    try {
+        userid = req.id;
+        updatedNote = {};
+        if (title) {
+            updatedNote.title = title;
+        }
+        if (description) {
+            updatedNote.description = description;
+        }
+        if (tag) {
+            updatedNote.tag = tag;
+        }
+        //checking that note is exist already and if it is exist already, Update the note
+        let note = await Note.findById(req.params.nid);
+        if (!note) {
+            return res.status(404).send("Note Not Found");
+        }
+        //checking that the user who is updating the note only that is him only
+        if (userid !== note.user.toString()) {
+            return res.status(401).send("Usermatch Not Found SO Not Allowed");
+        }
+
+        //find the note and update it
+        note = await Note.findByIdAndUpdate(req.params.nid, { $set: updatedNote }, { new: true });
+        res.json(note);
+    } catch (error) {
+        res.status(500).send("Some internal Server Error occurred, error: " + error.message);
+    }
+});
+
+// Delete user notes using: PUT "/api/notes/deletenote". LOGIN Required
+router.delete('/deletenote/:nid', getchusermiddleware, async (req, res) => {
+    userid = req.id;
+    try {
+
+        //checking that note is exist already and if it is exist already, Update the note
+        let note = await Note.findById(req.params.nid);
+        if (!note) {
+            return res.status(404).send("Note Not Found");
+        }
+        //checking that the user who is updating the note only that is him only
+        if (userid !== note.user.toString()) {
+            return res.status(401).send("Usermatch Not Found SO Not Allowed");
+        }
+
+        //find the note and update it
+        note = await Note.findByIdAndDelete(req.params.nid);
+        res.json({ Success: "Note has been Deleted", Note: note });
+    }
+    catch (error) {
         res.status(500).send("Some internal Server Error occurred, error: " + error.message);
     }
 });
